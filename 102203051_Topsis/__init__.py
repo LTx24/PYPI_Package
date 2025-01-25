fb2e648eed7011c12de5354483e1e12e6fde4b54
Lakshya Thakur <lakshyathakur24625@gmail.com>
2025-01-25 20:05:13 +0530

    Add files via upload

---
+++ 102203051_Topsis/__init__.py
@@ -0,0 +1,12 @@
+#Everthing you want to run when the module is initialized goes here
+
+from .Topsis_Lakshya_102203051 import (
+    convert_to_float,
+    normalize_decision_matrix,
+    calculate_distances,
+    calculate_topsis_scores,
+    topsis,
+    apply_weights,
+    calculate_ideal_best_worst,
+    run,
+)
